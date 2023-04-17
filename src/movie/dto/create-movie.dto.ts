0f117f0dfca9b6fff798e929fbe158d422fd2daf
Nikita Surov <surov.n2012@gmail.com>
2023-04-17 13:00:04 +0900

    Добавил дто для movie

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,1 +1,34 @@
-export class CreateMovieDto {}
+import { IsNotEmpty, IsNumber, IsObject, IsString } from 'class-validator';
+
+export class CreateMovieDto {
+    @IsNotEmpty()
+    @IsString({ message: 'Поле slug должно быть строкой' })
+    slug: string;
+
+    @IsString({ message: 'Поле poster должно быть строкой' })
+    poster: string;
+
+    @IsString({ message: 'Поле bigPoster должно быть строкой' })
+    bigPoster: string;
+
+    @IsNotEmpty()
+    @IsString({ message: 'Поле title должно быть строкой' })
+    title: string;
+
+    @IsString({ message: 'Поле videoUrl должно быть строкой' })
+    videoUrl: string;
+
+    @IsObject({ message: 'Поле parameters должно быть объектом' })
+    parameters: MovieParameters;
+}
+
+export class MovieParameters {
+    @IsNumber({}, { message: 'Поле year должно быть числом' })
+    year: number;
+
+    @IsNumber({}, { message: 'Поле duration должно быть числом' })
+    duration: number;
+
+    @IsString({ message: 'Поле country должно быть строкой' })
+    country;
+}
