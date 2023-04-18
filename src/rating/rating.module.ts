919e2888fed7ef49c5ba4f1654864830225dc16e
Nikita Surov <surov.n2012@gmail.com>
2023-04-18 11:02:03 +0900

    123

--- src/rating/rating.module.ts
+++ src/rating/rating.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RatingController } from './rating.controller';
 import { RatingService } from './rating.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { MovieService } from 'src/movie/movie.service';
+import { MovieModule } from 'src/movie/movie.module';
 
 @Module({
+    imports: [MovieModule],
     controllers: [RatingController],
-    providers: [RatingService, PrismaService, MovieService],
+    providers: [RatingService, PrismaService],
 })
 export class RatingModule {}
